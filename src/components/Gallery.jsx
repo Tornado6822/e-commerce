62c93cffce9bf49a1c728d6bef2a5064559aa8bb
Tornado6822 <morelpatrick97@gmail.com>
2026-06-29 23:25:43 -0400

    Finished V1 of Gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,46 @@
+import { Card } from "react-bootstrap";
 import "./Gallery.css";
+import { useState, useEffect } from "react";
 
 function Gallery() {
-  <div id="grid"></div>;
+  const [books, setBooks] = useState([]);
+  //Flag used because of delay fetching books
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    //async and await are used to ensure the code waits for the response before the next step.
+    async function loadData() {
+      try {
+        const response = await fetch("./books_with_covers.json");
+        const content = await response.json();
+        setBooks(content);
+        setLoading(false);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    }
+    loadData();
+  }, []);
+
+  return loading ? (
+    <h1>Page is loading.</h1>
+  ) : (
+    <div id="grid">
+      {books.map((book) => (
+        <div key={book.Title} className="card">
+          <img
+            src={book.thumbnail}
+            alt="No Thumbnail"
+            className="card-img-top"
+          />
+          <div className="card-body">
+            <h5 className="card-title">{book.Title}</h5>
+            <p className="card-text">{book.Authors}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Gallery;
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,10 @@
+import Gallery from "../components/Gallery";
+
 function Checkout() {
   return (
     <div>
       <h1>You are checking out</h1>
+      <Gallery />
     </div>
   );
 }
